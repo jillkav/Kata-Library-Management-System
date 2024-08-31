3c5679c0af383bd3e88419bc3aab70f62e9fd137
Jill Kava <jillkava2003@gmail.com>
2024-08-31 12:09:13 +0530

    Add tests and implementation for adding books

--- library-management-system/library.js
+++ library-management-system/library.js
@@ -1,3 +1,5 @@
+// library.js
+
 class Library {
     constructor() {
       this.books = [];
@@ -7,33 +9,9 @@ class Library {
       if (this.books.some(b => b.isbn === book.isbn)) {
         throw new Error('Book with this ISBN already exists.');
       }
-      book.borrowed = false; // Add a borrowed property
       this.books.push(book);
     }
-  
-    borrowBook(isbn) {
-      const book = this.books.find(b => b.isbn === isbn);
-      if (!book) {
-        throw new Error('Book not found.');
-      }
-      if (book.borrowed) {
-        throw new Error('Book is already borrowed.');
-      }
-      book.borrowed = true;
-    }
-
-    returnBook(isbn) {
-        const book = this.books.find(b => b.isbn === isbn);
-        if (!book) {
-          throw new Error('Book not found.');
-        }
-        if (!book.borrowed) {
-          throw new Error('Book is not borrowed.');
-        }
-        book.borrowed = false;
-      }
-
-      
   }
-
-  module.exports = Library;  
+  
+  module.exports = Library;
+  
